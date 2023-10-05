6068fdbeb1255e2f86b12e246b5ea4ec916e6efc
Mkrsun <mkrsun.dev@gmail.com>
2023-10-04 21:21:57 -0300

    Adding cors (default config)

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express, { Request, Response } from "express";
 import expressListRoutes from "express-list-routes";
 import { authRoutes } from "./auth/auth.routes";
 import { requireAcessTokenMiddleware } from "./middlewares/requireAcessToken.middleware";
+import cors from "cors";
 
 const app = express();
 const PORT = 3555;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
